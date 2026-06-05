67f3e75301e153b0d8f84493136fc8e53bb58a5b
flovatieno <flovatieno>
2026-06-05 17:03:30 +0300

    feat:create post table

---
+++ backend/pkg/db/migrations/sqlite/000004_create_posts_table.up.sql
@@ -0,0 +1,30 @@
+-- Migration: Create posts table
+-- Purpose: Store user posts
+
+
+CREATE TABLE IF NOT EXISTS posts (
+    id                TEXT PRIMARY KEY,   -- UUID stored as TEXT
+    author_id         TEXT NOT NULL,
+    content           TEXT NOT NULL,
+    image_url         TEXT,
+    privacy_setting   TEXT NOT NULL CHECK (
+        privacy_setting IN (
+            'public',
+            'almost_private',
+            'private'
+        )
+    ),
+    created_at        DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at        DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (author_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+);
+
+-- Indexes
+CREATE INDEX IF NOT EXISTS idx_posts_author
+ON posts(author_id);
+
+CREATE INDEX IF NOT EXISTS idx_posts_created
+ON posts(created_at);
